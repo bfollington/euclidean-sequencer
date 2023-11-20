58fe775c55df405031d0a937a665543c061a154c
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-11-20 14:47:58 +1000

    Add worms

---
+++ src/particles.ts
@@ -0,0 +1,54 @@
+import * as THREE from "three";
+
+const particlePositions: THREE.Vector3[] = [];
+const particleCount = 1000; // Adjust based on desired density
+const particleGeometry = new THREE.SphereGeometry(0.02, 4, 6); // Small spheres
+const particleMaterial = new THREE.MeshStandardMaterial({ color: 0xaaaaaa });
+
+const particleMesh = new THREE.InstancedMesh(
+  particleGeometry,
+  particleMaterial,
+  particleCount
+);
+
+export function addParticles(scene: THREE.Scene) {
+  scene.add(particleMesh);
+
+  for (let i = 0; i < particleCount; i++) {
+    const position = new THREE.Vector3(
+      (Math.random() - 0.5) * 10, // Adjust range as needed
+      (Math.random() - 0.5) * 10,
+      (Math.random() - 0.5) * 10
+    );
+
+    particlePositions.push(position);
+
+    const dummy = new THREE.Object3D();
+    dummy.position.copy(position);
+    dummy.updateMatrix();
+    particleMesh.setMatrixAt(i, dummy.matrix);
+  }
+
+  particleMesh.instanceMatrix.needsUpdate = true;
+}
+
+export function updateParticles() {
+  // Update particles
+  const time = performance.now() * 0.0001; // Time in seconds
+  particlePositions.forEach((position, i) => {
+    const dummy = new THREE.Object3D();
+    dummy.position.copy(position);
+
+    // Simple floating motion
+    dummy.position.y += Math.sin(time / 10.0 + i) * 5; // Adjust motion parameters as needed
+    dummy.position.z += Math.cos(time / 10.0 + i) * 5; // Adjust motion parameters as needed
+    dummy.scale.x = 0.1;
+    dummy.scale.y = 0.8;
+    dummy.scale.z = 0.5;
+    dummy.rotation.y = Math.cos(time + i) * Math.PI * 2;
+    dummy.updateMatrix();
+    particleMesh.setMatrixAt(i, dummy.matrix);
+  });
+
+  particleMesh.instanceMatrix.needsUpdate = true;
+}
--- src/scene.ts
+++ src/scene.ts
@@ -1,9 +1,93 @@
 import * as THREE from "three";
 import { addListener } from "./sequencer";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { addParticles, updateParticles } from "./particles";
+
+class Creature {
+  segments: THREE.Mesh[];
+  target: THREE.Object3D;
+  segmentDistance: number;
+  damping: number;
+  t: number = 0;
+
+  constructor(target: THREE.Object3D, color: string, segmentCount = 5) {
+    this.segments = [];
+    this.target = target; // The target cube that the head follows
+    this.segmentDistance = 0.2; // Distance between segments
+    this.damping = 0.05; // Damping for movement, adjust as needed
+
+    for (let i = 0; i < segmentCount; i++) {
+      const geometry = new THREE.SphereGeometry(
+        0.05 * (segmentCount - i),
+        16,
+        16
+      ); // Decrease size for each segment
+      const material = new THREE.MeshPhysicalMaterial({ color: color });
+      material.specularIntensity = 0.5;
+      material.clearcoat = 1;
+      const sphere = new THREE.Mesh(geometry, material);
+
+      // Position spheres along a line initially
+      sphere.position.x = i * -this.segmentDistance;
+      scene.add(sphere);
+      this.segments.push(sphere);
+    }
+
+    addListener((note, velocity) => {
+      // Check if there's a cube for the note
+      const orb = orbs[note];
+      if (orb && orb.cube == this.target) {
+        // Set the emissive color based on velocity
+        const mat = this.segments[0].material as THREE.MeshPhysicalMaterial;
+        mat.emissive.setHex(0xff00ff);
+        mat.emissiveIntensity = velocity;
+
+        // Fade out the emissive color
+        // (You might need to implement a more complex fading logic in your animation loop)
+        setTimeout(() => mat.emissive.setHex(0x000000), 500);
+      }
+    });
+  }
+
+  update() {
+    this.t++;
+    // Move head towards target
+    const head = this.segments[0];
+    const direction = new THREE.Vector3().subVectors(
+      this.target.position,
+      head.position
+    );
+    direction.multiplyScalar(this.damping);
+    head.position.add(direction);
+
+    // Make each segment follow its predecessor with a minimum distance
+    for (let i = 1; i < this.segments.length; i++) {
+      const segment = this.segments[i];
+      const previousSegment = this.segments[i - 1];
+
+      const followDirection = new THREE.Vector3().subVectors(
+        previousSegment.position,
+        segment.position
+      );
+      const distance = followDirection.length();
+
+      // Check if the distance is less than the minimum segment distance
+      if (distance < this.segmentDistance) {
+        // followDirection
+        //   .normalize()
+        //   .multiplyScalar(this.segmentDistance - distance);
+        // segment.position.add(followDirection);
+      } else {
+        followDirection.multiplyScalar(this.damping);
+        segment.position.add(followDirection);
+      }
+    }
+  }
+}
 
 // Basic Three.js setup
 const scene = new THREE.Scene();
+scene.fog = new THREE.Fog(0x0, 5, 10);
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -16,11 +100,11 @@ document.body.appendChild(renderer.domElement);
 new OrbitControls(camera, renderer.domElement);
 
 // Add ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+const ambientLight = new THREE.AmbientLight(0x0000ff, 0.5);
 scene.add(ambientLight);
 
 // Add directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+const directionalLight = new THREE.DirectionalLight(0xffdddd, 2);
 directionalLight.position.set(0, 1, 1);
 scene.add(directionalLight);
 
@@ -31,7 +115,7 @@ scene.add(directionalLight);
 // );
 // scene.add(boundingSphere);
 
-camera.position.z = 10;
+camera.position.z = 7;
 
 const colors = [
   "#355070",
@@ -49,7 +133,7 @@ const pentatonicScale = ["C4", "Eb4", "F4", "G4", "Bb4"];
 const pentatonicScaleOct = ["C5", "Eb5", "F5", "G5", "Bb5"];
 const notes = [...pentatonicScale, ...pentatonicScaleOct]; // Example notes
 let orbs: { [note: string]: NoteOrb } = {};
-const initialVelocity = 0.05; // Adjust as needed
+const initialVelocity = 0.025; // Adjust as needed
 const sphereRadius = 5;
 
 class NoteOrb {
@@ -87,8 +171,12 @@ notes.forEach((note, index) => {
 
   orbs[note] = orb;
   console.log(orb);
-  scene.add(mesh);
+  // scene.add(mesh);
 });
+
+const creatures = notes.map(
+  (note, idx) => new Creature(orbs[note].cube, colors[idx])
+);
 
 function animate() {
   requestAnimationFrame(animate);
@@ -110,8 +198,19 @@ function animate() {
     if (orb.cube.position.length() > sphereRadius) {
       orb.cube.position.normalize().multiplyScalar(sphereRadius);
       orb.velocity.negate(); // Invert velocity on collision
+      orb.velocity.add(
+        new THREE.Vector3(
+          (Math.random() - 0.5) * 0.07,
+          (Math.random() - 0.5) * 0.07,
+          (Math.random() - 0.5) * 0.07
+        )
+      );
+      orb.velocity.multiplyScalar(0.5); // Apply friction
     }
   });
+  updateParticles();
+
+  creatures.forEach((creature) => creature.update());
 
   renderer.render(scene, camera);
 }
@@ -127,7 +226,7 @@ addListener((note, velocity) => {
     mat.emissiveIntensity = velocity;
 
     // Apply acceleration based on velocity
-    const acceleration = velocity * 0.5; // Adjust multiplier as needed
+    const acceleration = velocity * 0.3; // Adjust multiplier as needed
     orb.velocity.add(
       new THREE.Vector3(
         (Math.random() - 0.5) * acceleration,
@@ -141,3 +240,5 @@ addListener((note, velocity) => {
     setTimeout(() => mat.emissive.setHex(0x000000), 500);
   }
 });
+
+addParticles(scene);
